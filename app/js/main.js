dc52d5aac0b8fd552db2c371a0e94d6d30ed231b
Alexander Kallaway <kallaway@me.com>
2016-08-13 02:09:06 -0400

    create search functionality, add ability to receive results with JSONP

--- app/js/main.js
+++ app/js/main.js
@@ -1,2 +1,38 @@
-var test = 3;
-console.log(test);
+"use strict";
+function handleJSONP(data) {
+	let results = data.query.search;
+	//buildResults(results);
+	console.log("Real information: ", data.query.search);
+};
+
+
+$(document).ready(function(){
+
+	// constants
+	const $resultsEl = $('#results'),
+	$searchBox = $('#search-box'),
+	$searchButton = $('#search-button'),
+	urlStart = "https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=",
+	urlEnd = "&format=json&callback=handleJSONP";
+
+
+
+	$searchButton.on('click', function(){
+		console.log("Click event happened on the main button.");
+		let searchText = $searchBox.val();
+		let requestedURL = urlStart + searchText + urlEnd;
+
+
+		var script = document.createElement("script");
+		script.src = requestedURL;
+	     // appendChild is calling the function
+	    document.body.appendChild(script);
+
+
+	});
+
+	var test = 3;
+	console.log(test);
+
+
+});
